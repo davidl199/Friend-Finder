f31cbd5074e84ca27ee496af1dbdd6d81cb1ac09
davidl199 <davidl1_99@yahoo.com>
2018-03-03 21:58:27 -0500

    corrected issues with api by setting extended property to true in bodyparser urlencoded finalized code

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,20 +4,30 @@
 
 var friendsArray = [
     {
-        "name": "Judy",
-        "photo": "https://randomuser.me/api/portraits/women/49.jpg",
-        "scores": [1,2,3] //,4,5,1,2,5,4,1]6
-     }//,
-    // {
-    //     "name": "Don",
-    //     "photo": "https://randomuser.me/api/portraits/men/42.jpg",
-    //     "scores": [2,1,3]
-    // },
-    // {
-    //     "name": "Jane",
-    //     "photo": "https://randomuser.me/api/portraits/women/21.jpg",
-    //     "scores": [3,1,1]
-    // }
+        name: "Judy",
+        photo: "https://randomuser.me/api/portraits/women/49.jpg",
+        scores: ['1','2','3','4','5','3','2','3','2','4']
+     },
+    {
+        name: "Don",
+        photo: "https://randomuser.me/api/portraits/men/42.jpg",
+        scores: ['2','1','4','1','2','3','4','5','3','2']
+    },
+    {
+        name: "Jane",
+        photo: "https://randomuser.me/api/portraits/women/21.jpg",
+        scores: ['1','3','1','5','3','4','2','2','2','4']
+    },
+    {
+        name: "Jack",
+        photo: "https://randomuser.me/api/portraits/men/91.jpg",
+        scores: ['1','3','1','5','5','5','5','2','3','4']
+    },
+    {
+        name: "Emma",
+        photo: "https://randomuser.me/api/portraits/women/30.jpg",
+        scores: ['1','1','1','5','5','5','5','1','3','4']
+    }
 ];
 
 module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,72 +1,49 @@
 // ===============================================================================
 // LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
 var friendsData = require("../data/friends");
-//var waitListData = require("../data/waitinglistData");
-
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
 module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
-
-  app.get("/api/admin", function(req, res) {
-    //res.json(tableData);
-  });
-
+ 
   // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
 
   app.post("/api/friends", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body-parser middleware
-    /* if (tableData.length < 5) {
-      tableData.push(req.body);
-      res.json(true);
+
+    var newFriend = req.body;
+    var arr = newFriend.scores;
+    var newFriendScore = 0
+    for( var i in arr) {
+       newFriendScore += parseInt(arr[i]);
     }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
-    } */
-    console.log("Post friends api");
-    friendsData.push(req.body);
-    console.log(friendsData);
-    //console.log(req.body);
-    // for(i=0;i < friendsData.length; i++) {
-    //   console.log(friendsData[i].scores[i])
-    // }
-    // console.log(friendsData[0].name + " " + friendsData[0].scores[0]);
-    // console.log(friendsData[0].name + " " + friendsData[0].scores[1]);
-    // console.log(friendsData[0].name + " " + friendsData[0].scores[2]);
-
-    // console.log(friendsData[1].name + " " + friendsData[1].scores[0]);
-    res.json(friendsData[0]);
-  });
-
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-
-  app.post("/api/clear", function() {
-    // Empty out the arrays of data
-    /* tableData = [];
-    waitListData = [];
-
-    console.log(tableData); */
+   
+    var scoreTotal = 0
+    var bestScore = 100;
+    var frndArray = [];
+    var frnd = null;
+    var match = friendsData[0];
+    var diff = 0;
+    for(i = 0; i < friendsData.length; i++) {
+       frnd = friendsData[i];
+       frndArray = friendsData[i].scores;
+       for(var x in friendsData[i].scores) {
+           scoreTotal += parseInt(frndArray[x]);
+       }
+       diff = newFriendScore - scoreTotal;
+       diff = Math.abs(diff);
+       if(diff < bestScore) {
+           bestScore = diff;
+           match = frnd;
+       }
+       scoreTotal = 0;
+    }
+  
+    friendsData.push(newFriend);
+    res.json(match);
   });
 };
